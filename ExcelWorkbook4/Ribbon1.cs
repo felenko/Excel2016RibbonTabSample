2e96b0c0d1e0a7f5282cd07e7b007938a941baeb
Kostiantyn Felenko <felenko@gmai.coml>
2017-07-21 23:56:57 -0400

    Initial Commit

---
+++ ExcelWorkbook4/Ribbon1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace ExcelWorkbook4
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+    }
+}
